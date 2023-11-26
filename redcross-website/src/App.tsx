84a3dca0304bda408d78fc50392cdfe5011b728f
YashChavan999 <yaash.chavan@gmail.com>
2023-11-26 13:41:36 -0700

    Added DOM Router

--- redcross-website/src/App.tsx
+++ redcross-website/src/App.tsx
@@ -1,5 +1,15 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+
 function App() {
-	return <></>;
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
+		</>
+	);
 }
 
 export default App;
