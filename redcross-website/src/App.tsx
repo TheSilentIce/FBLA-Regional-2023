dae19d6234d704dd3a6d98330c453bb41261c0db
TheSilentIce <srikaryadlapati@gmail.com>
2023-12-07 19:19:49 -0700

    Left side of the donate page done

--- redcross-website/src/App.tsx
+++ redcross-website/src/App.tsx
@@ -3,17 +3,19 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Register } from "./pages/Register";
 import Navbar from "./components/Navbar";
+import Donate from "./pages/Donate";
 
 function App() {
-	return (
-		<>
-			<Navbar />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/register" element={<Register />} />
-			</Routes>
-		</>
-	);
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/donate" element={<Donate />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
